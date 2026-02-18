c3f3c3a9302daf57859fc188019745fef7172051
Tyler S <78190945+T28ST@users.noreply.github.com>
2026-02-18 11:40:46 -0500

    rearrange

--- app/(tabs)/three.tsx
+++ app/(tabs)/three.tsx
@@ -8,12 +8,12 @@ export default function TabThreeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Three</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Link href="/modal" asChild>
         <Pressable>
             asChild Link
         </Pressable>
       </Link>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -8,10 +8,10 @@ export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Pressable onPress={ () => router.push('/modal')}>
         Pressable
       </Pressable>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
