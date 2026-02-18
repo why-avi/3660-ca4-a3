a6dc4d145ea6d9d3f897f3e21191c151f0911413
Tyler S <78190945+T28ST@users.noreply.github.com>
2026-02-18 11:37:55 -0500

    small change

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -9,10 +9,10 @@ export default function TabTwoScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
       <Pressable onPress={ () => router.push('/modal')}>
         Pressable
       </Pressable>
+      <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
